60c066e5df19369090d11d59f04ce8e90505798d
mlvzk <mlvzk@protonmail.com>
2019-04-22 15:39:14 +0200

    fix unicode

--- drw.c
+++ drw.c
@@ -21,7 +21,7 @@ utf8decodebyte(const char c, size_t *i)
 {
 	for (*i = 0; *i < (UTF_SIZ + 1); ++(*i))
 		if (((unsigned char)c & utfmask[*i]) == utfbyte[*i])
-			return (unsigned char)c & ~utfmask[*i];
+			return (unsigned char)c;
 	return 0;
 }
 
